41e681ad0c32640c4bae28423926da117723bdfb
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-01-05 21:34:09 +0530

    Update MergePointLinkedLists.java

--- CompetitiveCoding/LinkedList/MergePointLinkedLists.java
+++ CompetitiveCoding/LinkedList/MergePointLinkedLists.java
@@ -10,58 +10,25 @@
 //
 // Author: Akash Yadav
 
-class Node {
-    int data;
-    Node next;
+static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
 
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
+    SinglyLinkedListNode a = head1;
+    SinglyLinkedListNode b = head2;
 
-class GfG {
-  
-    static Node intersectPoint(Node head1, Node head2) {
+    // keep moving until both pointers meet
+    while (a != b) {
 
-        // iterate over second list and for each node
-        // Search it in first list
-        while (head2 != null) {
-            Node temp = head1;
-            while (temp != null) {
-
-                // if both Nodes are same
-                if (temp == head2)
-                    return head2;
-                temp = temp.next;
-            }
-            head2 = head2.next;
-        }
-
-        // intersection is not present between the lists
-        return null;
-    }
-
-    public static void main(String[] args) {
-
-        // creation of first list: 10 -> 15 -> 30
-        Node head1 = new Node(10);
-        head1.next = new Node(15);
-        head1.next.next = new Node(30);
-
-        // creation of second list: 3 -> 6 -> 9 -> 15 -> 30
-        Node head2 = new Node(3);
-        head2.next = new Node(6);
-        head2.next.next = new Node(9);
-
-        // 15 is the intersection point
-        head2.next.next.next = head1.next;
-
-        Node intersectionPoint = intersectPoint(head1, head2);
-
-        if (intersectionPoint == null)
-            System.out.println("-1");
+        if (a == null)
+            a = head2;
         else
-            System.out.println(intersectionPoint.data);
+            a = a.next;
+
+        if (b == null)
+            b = head1;
+        else
+            b = b.next;
     }
+
+    // a (or b) is the merge point
+    return a.data;
 }
